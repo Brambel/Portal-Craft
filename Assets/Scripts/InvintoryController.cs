f86d3ef246df2ab5db3c0d7e5ee2f480643901b3
JameseEbert <jamesebert1@gmail.com>
2017-09-05 14:47:02 -0500

    basic invintory functionality
    added invintory controller, it auto populates a couple items at the
    moment for testing.

---
+++ Assets/Scripts/InvintoryController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InvintoryController : MonoBehaviour {
+
+    public GameObject item;
+    public GameObject invintoryPanel;
+
+    private List<GameObject> items = new List<GameObject>();
+
+	void Awake () {
+        for (int i = 0; i < 3; ++i) {
+            items.Add(Instantiate(item, new Vector2(0, (-50 * i)+190),item.transform.rotation));
+            Debug.Log(items[i]);
+            items[i].transform.SetParent(invintoryPanel.transform,false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/NavigationController.cs
+++ Assets/Scripts/NavigationController.cs
@@ -6,27 +6,37 @@ using UnityEngine.UI;
 public class NavigationController : MonoBehaviour {
 
     public Button[] buttons;
-
+    public GameObject[] splashes; //TODO: renam
 
     public void Start() {
-        
+        //clear all buttons
+        foreach (GameObject g in splashes) {
+            g.SetActive(false);
+        }
+
+        mainMenuClicked();
     }
 
     public void mainMenuClicked() {
-        clearButtons();
+        clearScreen();
         buttons[0].interactable = false;
         Debug.Log("we're on main menu");
     }
 
     public void invintoryClicked() {
-        clearButtons();
+        clearScreen();
         buttons[1].interactable = false;
         Debug.Log("we're on invintory");
+        splashes[0].SetActive(true);
     }
 
-    private void clearButtons(){
+    private void clearScreen(){
         foreach (Button b in buttons) {
             b.interactable = true;
+        }
+
+        foreach (GameObject g in splashes) {
+            g.SetActive(false);
         }
     }
 }
