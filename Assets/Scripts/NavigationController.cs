36d282f12dace822182e445d391d2753d2f21623
JameseEbert <jamesebert1@gmail.com>
2017-09-04 19:51:12 -0500

    basic navigation menu added
    main menu and invintory added to navigation menu, both have basic
    functionality.

---
+++ Assets/Scripts/NavigationController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NavigationController : MonoBehaviour {
+
+    public Button[] buttons;
+
+
+    public void Start() {
+        
+    }
+
+    public void mainMenuClicked() {
+        clearButtons();
+        buttons[0].interactable = false;
+        Debug.Log("we're on main menu");
+    }
+
+    public void invintoryClicked() {
+        clearButtons();
+        buttons[1].interactable = false;
+        Debug.Log("we're on invintory");
+    }
+
+    private void clearButtons(){
+        foreach (Button b in buttons) {
+            b.interactable = true;
+        }
+    }
+}
